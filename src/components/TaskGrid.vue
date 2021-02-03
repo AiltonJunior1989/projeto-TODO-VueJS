f8028838962d35b962531cfc9544a0e74b5a9273
Ailton Junior <ailtonjesusjunior@gmail.com>
2021-02-03 10:19:14 -0300

    Criando repositório do Projeto TODO

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="(task, i) in tasks" :key="task.name"
+                @taskDeleted="$emit('taskDeleted', i)"
+                @taskStateChanged="$emit('taskStateChanged', i)"
+                :task="task"></Task>
+        </template>
+        <p v-else class="no-task">Sua vida esta em dia &#x1F61C;</p>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+    components: { Task },
+    props: {
+        tasks: { type: Array, required: true }
+    }
+}
+</script>
+
+<style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #aaa;
+        font-size: 2rem;
+        text-shadow: 0px 2px 20px salmon;
+    }
+</style>
